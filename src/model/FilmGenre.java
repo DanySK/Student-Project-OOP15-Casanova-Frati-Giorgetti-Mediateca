be1b39943e2bab3bc93ab0350f9dd023809023c5
Edoardo Frati <edoardofrati95@gmail.com>
2016-04-18 12:45:40 +0200

    Enumeration of film genre

---
+++ src/model/FilmGenre.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * Enum of film review taken from 
+ * https://it.wikipedia.org/wiki/Generi_cinematografici
+ * @author Edoardo
+ *
+ */
+public enum FilmGenre {
+	
+	ANIMAZIONE,
+	AVVENTURA,
+	BIOGRAFICO,
+	COMMEDIA,
+	DOCUMENTARIO,
+	DRAMMATICO,
+	FANTASIENZA,
+	FANTASY,
+	GUERRA,
+	HORROR,
+	MUSICAL,
+	STORICO,
+	THRILLER,
+	WESTERN
+
+}
