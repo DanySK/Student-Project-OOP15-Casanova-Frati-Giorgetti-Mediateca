7b0c6f39e1f511a9a7f4e48f5856b82ab5049031
Edoardo Frati <edoardofrati95@gmail.com>
2016-04-18 12:47:49 +0200

    General item sterter implementation

---
+++ src/model/Item_impl.java
@@ -0,0 +1,14 @@
+package model;
+import java.awt.Image;
+import java.util.List;
+
+import com.google.common.base.Optional;
+
+public class Item_impl {
+
+	String title;
+	Optional<Image> cover;
+	Optional<List<Review>> set_review;
+	int like;
+	
+}
