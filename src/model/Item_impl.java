325402eded7803b19f5aa986e6cc394e47c0f910
Edoardo Frati <edoardo.frati2@studio.unibo.it>
2016-04-19 09:32:32 +0200

    Google Guava Library

--- src/model/Item_impl.java
+++ src/model/Item_impl.java
@@ -1,40 +1,39 @@
 package model;
+
 import java.awt.Image;
 import java.util.List;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 
-public class Item_impl implements Item{
+public class Item_impl implements Item {
 
-	String title;
-	int release_year;
-	String author;
-	Optional<Image> cover;
-	Optional<List<Review>> set_review;
-	int like;
-	
+	private String title;
+	private int release_year;
+	private String author;
+	private Optional<Image> cover;
+	private Optional<List<Review>> set_review;
+	private int like;
+
 	/**
-	 * hashCode function uses the Objects.hashCode( field1, field2, .. ) 
-	 * taken from Google Guava
+	 * hashCode function uses the Objects.hashCode( field1, field2, .. ) taken
+	 * from Google Guava
 	 */
 	@Override
-	public int hashCode(){
-		return Objects.hashCode(author, release_year, title);
+	public int hashCode() {
+		return Objects.hashCode(this.author, this.release_year, this.title);
 	}
-	
+
 	/**
-	 * equals function uses the Objects.equal(...)
-	 * taken from Google Guava
+	 * equals function uses the Objects.equal(...) taken from Google Guava
 	 */
 	@Override
-	public boolean equals(Object object) {
-	      if(!(object instanceof Item_impl) || object == null){
-	          return false;
-	       }
-	      Item_impl item = (Item_impl)object;
-	       return Objects.equal(author, item.author)
-	    	&& Objects.equal(release_year, item.release_year)
-	        && Objects.equal(title, item.title);
-	    }
+	public boolean equals(final Object object) {
+		if (!(object instanceof Item_impl) || (object == null)) {
+			return false;
+		}
+		final Item_impl item = (Item_impl) object;
+		return Objects.equal(this.author, item.author) && Objects.equal(this.release_year, item.release_year)
+				&& Objects.equal(this.title, item.title);
+	}
 }
