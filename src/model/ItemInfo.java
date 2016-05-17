5ba2ff447d19b10cdef0eefdd0670cec5688554f
Edoardo Frati <edoardo.frati2@studio.unibo.it>
2016-05-17 15:13:10 +0200

    Fixing methods

--- src/model/ItemInfo.java
+++ src/model/ItemInfo.java
@@ -1,17 +1,17 @@
 package model;
 
 import java.util.GregorianCalendar;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ItemInfo {
 
     private Integer quantity;
-    private List<Pair<Integer, GregorianCalendar>> userList;
+    private Map<Integer, GregorianCalendar> userList;
 
     public ItemInfo(final int initQuantity) {
         this.quantity = initQuantity;
-        this.userList = new LinkedList<Pair<Integer, GregorianCalendar>>();
+        this.userList = new HashMap<>();
     }
 
     public Integer getQuantity() {
@@ -22,20 +22,54 @@ public class ItemInfo {
         this.quantity = this.quantity + initQuantity;
     }
 
-    public List<Pair<Integer, GregorianCalendar>> getUserList() {
+    public Map<Integer, GregorianCalendar> getUserList() {
         return this.userList;
     }
 
-    public void setUserList(final List<Pair<Integer, GregorianCalendar>> userList) {
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result) + ((this.quantity == null) ? 0 : this.quantity.hashCode());
+        result = (prime * result) + ((this.userList == null) ? 0 : this.userList.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        ItemInfo other = (ItemInfo) obj;
+        if (this.quantity == null) {
+            if (other.quantity != null) {
+                return false;
+            }
+        } else if (!this.quantity.equals(other.quantity)) {
+            return false;
+        }
+        if (this.userList == null) {
+            if (other.userList != null) {
+                return false;
+            }
+        } else if (!this.userList.equals(other.userList)) {
+            return false;
+        }
+        return true;
+    }
+
+    public void setQuantity(final Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUserList(final Map<Integer, GregorianCalendar> userList) {
         this.userList = userList;
     }
 
-    public int getUserPosition(final Integer userId) {
-        for (int i = 0; i < this.userList.size(); i++) {
-            if (i == userId) {
-                return i;
-            }
-        }
-        return -1;
-    }
 }
