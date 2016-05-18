482eab1fef857bc060b90ada757e64b698ab8ac1
Edoardo Frati <edoardo.frati2@studio.unibo.it>
2016-05-18 11:17:40 +0200

    Empty class to clarify dir

---
+++ src/controller/GoodJob.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class GoodJob {
+
+    public static void main(final String[] args) {
+        System.out.println("Good Job Controller");
+    }
+
+}
---
+++ src/view/GoodJob.java
@@ -0,0 +1,9 @@
+package view;
+
+public class GoodJob {
+
+    public static void main(final String[] args) {
+        System.out.println("Good Job View");
+    }
+
+}
