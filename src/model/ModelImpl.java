38ebb866b5ee2e5874a3142267b8e2d63d146f1a
Edoardo Frati <edoardo.frati2@studio.unibo.it>
2016-05-23 12:07:24 +0200

    Adding contains and deleteItem methods

---
+++ src/model/ModelImpl.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.io.Serializable;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import model.item.ArchiveImpl;
+import model.item.BookGenre;
+import model.item.ItemFactory;
+import model.item.Language;
+import model.item.MovieGenre;
+import model.user.ArchiveUser;
+
+public class ModelImpl implements Serializable, Model {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -8370710936091204583L;
+    private ArchiveImpl archiveItem = ArchiveImpl.getArchiveImpl();
+    private ArchiveUser archiveUser = ArchiveUser.getArchiveImpl();
+
+    @Override
+    public void registerUser(final String initName, final String initSurname, final GregorianCalendar initBirthdate,
+            final String initUsername, final String initPassword, final String initEmail,
+            final String initTelephoneNumber, final List<BookGenre> initBookPref,
+            final List<MovieGenre> initMoviePref) {
+
+    }
+
+    @Override
+    public void deleteUser(final int userId) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void registerBook(final String initTitle, final int initReleaseYear, final String initAuthor,
+            final Language initCurrentLanguage, final String initISBN, final BookGenre initGenre,
+            final String initPublisher, final Integer initNumRelease, final Integer initNumCopy) throws Exception {
+        this.archiveItem.addItem(ItemFactory.getNewBook(initTitle, initReleaseYear, initAuthor, initCurrentLanguage,
+                initISBN, initGenre, initPublisher, initNumRelease), initNumCopy);
+
+    }
+
+    @Override
+    public void registerMovie(final String initTitle, final int initReleaseYear, final String initPublisher,
+            final String initAuthor, final Language initCurrentLanguage, final MovieGenre initGenre,
+            final Integer initDuration, final Boolean initColor, final Integer initNumCopy) throws Exception {
+        this.archiveItem.addItem(ItemFactory.getNewMovie(initTitle, initReleaseYear, initPublisher, initAuthor,
+                initCurrentLanguage, initGenre, initDuration, initColor), initNumCopy);
+    }
+
+    @Override
+    public void deleteItem(final int itemId) throws Exception {
+        if (this.archiveItem.contains(itemId)) {
+            this.archiveItem.removeItem(itemId);
+        } else {
+            throw new Exception("Item: " + itemId + " is not into the archive.");
+        }
+
+    }
+
+    @Override
+    public void borrowItem(final int itemId, final int userId) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void returnItem(final int itemId, final int userId) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
