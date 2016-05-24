c89e0cdfc6b15dbf216cb3c0122ae0ab4c8491d9
Enrico Casanova <enrico.casanova@studio.unibo.it>
2016-05-24 10:16:50 +0200

    modified Application

--- src/controller/Application.java
+++ src/controller/Application.java
@@ -4,5 +4,14 @@ public class Application {
 
 	public static void main(final String[] args) {
 
+		// application starter
+		final Controller c = new ControllerImpl();
+		// temporary starting instructions
+
+		// final View v = new ViewImpl(c);
+		// final Model m = new ModelImpl();
+		// c.setModel(m);
+		// c.setView(v);
+		// v.startView();
 	}
 }
---
+++ src/controller/ControllerImpl.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class ControllerImpl implements Controller {
+	@Override
+	public void login(final String userName, final String password) {
+
+	}
+}
