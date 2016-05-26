11f18ca9189db0bec637661faf348e94e73fe7c5
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-05-26 14:57:13 +0200

    Added empty Menu and MenuImpl

---
+++ src/view/Menu.java
@@ -0,0 +1,12 @@
+package view;
+
+/**
+ *
+ * Interface for the menu page.
+ *
+ * @author Luca Giorgetti
+ *
+ */
+public interface Menu {
+
+}
---
+++ src/view/MenuImpl.java
@@ -0,0 +1,11 @@
+package view;
+
+/**
+ * MenuImpl contains the implementation of methods for the Menu page.
+ *
+ * @author Luca Giorgetti
+ *
+ */
+public class MenuImpl implements Menu {
+
+}
