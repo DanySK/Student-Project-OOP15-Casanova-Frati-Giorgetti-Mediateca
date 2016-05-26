2338be5a10c6c3f4e5e218ce305c925a8f240e5a
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-05-26 15:36:04 +0200

    Changes for Enrico, which doesn't understands

--- src/view/MenuImpl.java
+++ src/view/MenuImpl.java
@@ -1,5 +1,8 @@
 package view;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 /**
  * MenuImpl contains the implementation of methods for the Menu page.
  *
@@ -7,5 +10,7 @@ package view;
  *
  */
 public class MenuImpl implements Menu {
+	private JPanel menuPanel = new JPanel();
+	private JButton exitProgram = new JButton();
 
 }
