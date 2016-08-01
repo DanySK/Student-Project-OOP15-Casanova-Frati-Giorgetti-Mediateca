29a3f860ee6d801511beec93c62093ef08fded54
Edoardo Frati <edoardo.frati2@studio.unibo.it>
2016-08-01 11:46:22 +0200

    Fixing checkstyle

--- src/model/item/ArchiveImpl.java
+++ src/model/item/ArchiveImpl.java
@@ -20,190 +20,204 @@ import model.Pair;
  */
 public final class ArchiveImpl implements Serializable, Archive {
 
+  /**
+   * SSINDD is the double used to divide milliseconds and get the days.
+   */
+  private static final double SSINDD = 86400000.0;
+  private static final long serialVersionUID = 3943672353334594237L;
+  private static ArchiveImpl singleton = null;
+  private Map<Integer, Pair<ItemImpl, ItemInfo>> itemArchive = new HashMap<>();
+
+  /**
+   * Simple enum in order to know if item is a book or a movie.
+   *
+   * @author Edoardo
+   *
+   */
+  public enum TypeItem {
     /**
-     * SSINDD is the double used to divide milliseconds and get the days.
+     * Item's types.
      */
-    private static final double SSINDD = 86400000.0;
-    private static final long serialVersionUID = 3943672353334594237L;
-    private static ArchiveImpl singleton = null;
-    private Map<Integer, Pair<ItemImpl, ItemInfo>> itemArchive = new HashMap<>();
+    MOVIE, BOOK
+  }
 
-    public enum TypeItem {
-        MOVIE, BOOK
+  /**
+   * Empty private constructor. NON COMPLETO DEVE ESSERE AGGIUNTO IL CASO IN CUI
+   * L'ARCHIVIO HA GIA' UN FILE DI CONFIGURAZIONE.
+   */
+  private ArchiveImpl() {
+  }
+
+  /**
+   * Singleton constructor.
+   *
+   * @return the ArchiveImpl.
+   */
+  public static ArchiveImpl getArchiveImpl() {
+    if (ArchiveImpl.singleton == null) {
+      ArchiveImpl.singleton = new ArchiveImpl();
+    }
+    return ArchiveImpl.singleton;
+  }
+
+  @Override
+  public void addItem(final ItemImpl i, final Integer initNumCopy) throws Exception {
+    if (initNumCopy > 0) {
+      if (!this.itemArchive.containsKey(i.getiD())) {
+        this.itemArchive.put(i.getiD(), new Pair<>(i, new ItemInfo(initNumCopy)));
+      } else {
+        this.changeAmount(i.getiD(), initNumCopy);
+      }
+    } else {
+      throw new RuntimeException("initNumCopy <= 0");
+    }
+  }
+
+  @Override
+  public void changeAmount(final Integer itemId, final Integer amount) throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      this.itemArchive.get(itemId).getSecond().addQuantity(amount);
+    } else {
+      throw new Exception("Item " + itemId + " is not in the archive.");
     }
 
-    /**
-     * Empty private constructor. NON COMPLETO DEVE ESSERE AGGIUNTO IL CASO IN
-     * CUI L'ARCHIVIO HA GIA' UN FILE DI CONFIGURAZIONE.
-     */
-    private ArchiveImpl() {
+  }
+
+  @Override
+  public Item getItem(final Integer itemId) throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      if (Book.class.isInstance(this.itemArchive.get(itemId).getFirst())) {
+        return this.itemArchive.get(itemId).getFirst();
+      }
+      if (Movie.class.isInstance(this.itemArchive.get(itemId).getFirst())) {
+        return this.itemArchive.get(itemId).getFirst();
+      }
+    } else {
+      throw new Exception("Item " + itemId + "is not in the archive.");
+    }
+    return null;
+  }
+
+  @Override
+  public void removeItem(final Integer itemId) throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      this.itemArchive.remove(itemId);
+    } else {
+      throw new Exception("Item: " + itemId + " is not into the archive.");
     }
 
-    /**
-     * Singleton constructor.
-     *
-     * @return the ArchiveImpl.
-     */
-    public static ArchiveImpl getArchiveImpl() {
-        if (ArchiveImpl.singleton == null) {
-            ArchiveImpl.singleton = new ArchiveImpl();
-        }
-        return ArchiveImpl.singleton;
+  }
+
+  @Override
+  public double calculateDifferenceDays(final Integer itemId, final Integer userId)
+              throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      if (this.itemArchive.get(itemId).getSecond().getUserList().containsKey(userId)) {
+        return this.dayBetweenDates(
+                    this.itemArchive.get(itemId).getSecond().getUserList().get(userId));
+      } else {
+        throw new Exception("User: " + userId + "Not contained into the " + itemId + " list");
+      }
+    } else {
+      throw new Exception("Item: " + itemId + "Not contained into the archive.");
     }
+  }
 
-    @Override
-    public void addItem(final ItemImpl i, final Integer initNumCopy) throws Exception {
-        if (initNumCopy > 0) {
-            if (!this.itemArchive.containsKey(i.getiD())) {
-                this.itemArchive.put(i.getiD(), new Pair<>(i, new ItemInfo(initNumCopy)));
-            } else {
-                this.changeAmount(i.getiD(), initNumCopy);
-            }
-        } else {
-            throw new RuntimeException("initNumCopy <= 0");
-        }
-    }
+  private double dayBetweenDates(final GregorianCalendar fromDate) {
 
-    @Override
-    public void changeAmount(final Integer itemId, final Integer amount) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            this.itemArchive.get(itemId).getSecond().addQuantity(amount);
-        } else {
-            throw new Exception("Item " + itemId + " is not in the archive.");
-        }
+    GregorianCalendar toDate = this.getToDay();
 
-    }
+    // Conversion in ms
+    long msFromDate = fromDate.getTimeInMillis();
+    long msToDate = toDate.getTimeInMillis();
 
-    @Override
-    public Item getItem(final Integer itemId) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            if (Book.class.isInstance(this.itemArchive.get(itemId).getFirst())) {
-                return this.itemArchive.get(itemId).getFirst();
-            }
-            if (Movie.class.isInstance(this.itemArchive.get(itemId).getFirst())) {
-                return this.itemArchive.get(itemId).getFirst();
-            }
-        } else {
-            throw new Exception("Item " + itemId + "is not in the archive.");
-        }
-        return null;
-    }
+    long msBetween = msToDate - msFromDate;
 
-    @Override
-    public void removeItem(final Integer itemId) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            this.itemArchive.remove(itemId);
-        } else {
-            throw new Exception("Item: " + itemId + " is not into the archive.");
-        }
+    // Conversion in days with math rounding
+    double ddBetween = Math.round(msBetween / ArchiveImpl.SSINDD);
 
-    }
+    return ddBetween;
+  }
 
-    @Override
-    public double calculateDifferenceDays(final Integer itemId, final Integer userId) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            if (this.itemArchive.get(itemId).getSecond().getUserList().containsKey(userId)) {
-                return this.dayBetweenDates(this.itemArchive.get(itemId).getSecond().getUserList().get(userId));
-            } else {
-                throw new Exception("User: " + userId + "Not contained into the " + itemId + " list");
-            }
-        } else {
-            throw new Exception("Item: " + itemId + "Not contained into the archive.");
-        }
-    }
-
-    private double dayBetweenDates(final GregorianCalendar fromDate) {
-
-        GregorianCalendar toDate = this.getToDay();
-
-        // Conversion in ms
-        long msFromDate = fromDate.getTimeInMillis();
-        long msToDate = toDate.getTimeInMillis();
-
-        long msBetween = msToDate - msFromDate;
-
-        // Conversion in days with math rounding
-        double ddBetween = Math.round(msBetween / ArchiveImpl.SSINDD);
-
-        return ddBetween;
-    }
-
-    private GregorianCalendar getToDay() {
-        // Current date
-        Calendar c = Calendar.getInstance();
-        GregorianCalendar toDay = new GregorianCalendar(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
+  private GregorianCalendar getToDay() {
+    // Current date
+    Calendar c = Calendar.getInstance();
+    GregorianCalendar toDay = new GregorianCalendar(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
                 c.get(Calendar.DAY_OF_MONTH));
-        return toDay;
-    }
+    return toDay;
+  }
 
-    @Override
-    public void addUser(final Integer itemId, final Integer userId) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            this.itemArchive.get(itemId).getSecond().getUserList().put(userId, this.getToDay());
-            System.out.println("User " + userId + " adds to book list " + itemId + " in date " + this.getToDay());
-        } else {
-            throw new Exception("User: " + userId + "can not take item: " + itemId
-                    + "becouse it is not contained into the archive.");
-        }
+  @Override
+  public void addUser(final Integer itemId, final Integer userId) throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      this.itemArchive.get(itemId).getSecond().getUserList().put(userId, this.getToDay());
+      System.out.println("User " + userId + " adds to book list " + itemId + " in date "
+                  + this.getToDay());
+    } else {
+      throw new Exception("User: " + userId + "can not take item: " + itemId
+                  + "becouse it is not contained into the archive.");
     }
+  }
 
-    @Override
-    public void removeUser(final Integer itemId, final Integer userId) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            if (this.itemArchive.get(itemId).getSecond().getUserList().containsKey(userId)) {
-                this.itemArchive.get(itemId).getSecond().getUserList().remove(userId, this.getToDay());
-                System.out.println("User " + userId + " adds to item list " + itemId + " in date " + this.getToDay());
-            } else {
-                throw new Exception(("userId is not in the item's list"));
-            }
-        } else {
-            throw new Exception(
-                    "User: " + userId + "can not remove user:" + userId + " from item: " + itemId + "list.");
-        }
+  @Override
+  public void removeUser(final Integer itemId, final Integer userId) throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      if (this.itemArchive.get(itemId).getSecond().getUserList().containsKey(userId)) {
+        this.itemArchive.get(itemId).getSecond().getUserList().remove(userId, this.getToDay());
+        System.out.println("User " + userId + " adds to item list " + itemId + " in date "
+                    + this.getToDay());
+      } else {
+        throw new Exception(("userId is not in the item's list"));
+      }
+    } else {
+      throw new Exception("User: " + userId + "can not remove user:" + userId + " from item: "
+                  + itemId + "list.");
     }
+  }
 
-    @Override
-    public boolean checkAvailability(final Integer itemId) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            return this.itemArchive.get(itemId).getSecond().isAvailable();
-        } else {
-            throw new Exception("Item: " + itemId + "Not contained into the archive.");
-        }
+  @Override
+  public boolean checkAvailability(final Integer itemId) throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      return this.itemArchive.get(itemId).getSecond().isAvailable();
+    } else {
+      throw new Exception("Item: " + itemId + "Not contained into the archive.");
     }
+  }
 
-    @Override
-    public Set<Integer> getUserList(final Integer itemId) throws Exception {
-        if (this.itemArchive.containsKey(itemId)) {
-            Set<Integer> ls = new HashSet<>(this.itemArchive.get(itemId).getSecond().getUserList().keySet());
-            return ls;
-        } else {
-            throw new Exception("Item: " + itemId + "Not contained into the archive.");
-        }
+  @Override
+  public Set<Integer> getUserList(final Integer itemId) throws Exception {
+    if (this.itemArchive.containsKey(itemId)) {
+      Set<Integer> ls = new HashSet<>(
+                  this.itemArchive.get(itemId).getSecond().getUserList().keySet());
+      return ls;
+    } else {
+      throw new Exception("Item: " + itemId + "Not contained into the archive.");
     }
+  }
 
-    @Override
-    public boolean contains(final Integer itemId) {
-        return (this.itemArchive.containsKey(itemId));
-    }
+  @Override
+  public boolean contains(final Integer itemId) {
+    return (this.itemArchive.containsKey(itemId));
+  }
 
-    @Override
-    public Set<Integer> getItemId(final TypeItem t) {
-        Set<Integer> booklist = new HashSet<>();
-        Set<Integer> movielist = new HashSet<>();
-        for (Pair<ItemImpl, ItemInfo> i : this.itemArchive.values()) {
-            if (Book.class.isInstance(i.getFirst())) {
-                booklist.add(i.getFirst().getiD());
-            }
-            if (Movie.class.isInstance(i.getFirst())) {
-                movielist.add(i.getFirst().getiD());
-            }
-        }
-        if (t == TypeItem.BOOK) {
-            return booklist;
-        }
-        if (t == TypeItem.MOVIE) {
-            return movielist;
-        }
-        return null;
+  @Override
+  public Set<Integer> getItemId(final TypeItem t) {
+    Set<Integer> booklist = new HashSet<>();
+    Set<Integer> movielist = new HashSet<>();
+    for (Pair<ItemImpl, ItemInfo> i : this.itemArchive.values()) {
+      if (Book.class.isInstance(i.getFirst())) {
+        booklist.add(i.getFirst().getiD());
+      }
+      if (Movie.class.isInstance(i.getFirst())) {
+        movielist.add(i.getFirst().getiD());
+      }
     }
+    if (t == TypeItem.BOOK) {
+      return booklist;
+    }
+    if (t == TypeItem.MOVIE) {
+      return movielist;
+    }
+    return null;
+  }
 }
--- src/model/item/ItemImpl.java
+++ src/model/item/ItemImpl.java
@@ -17,148 +17,155 @@ import com.google.common.base.Objects;
  */
 public class ItemImpl implements Item, Serializable {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 7331548557628545541L;
-    private final int iD;
-    private final String title;
-    private final int releaseYear;
-    private final String publisher;
-    private final String author;
-    private final Language currentLanguage;
-    private List<ReviewImpl> setReview = new LinkedList<ReviewImpl>();
-    private Set<Integer> like;
-    private ItemGenre genre;
-    private float averageVote;
+  /**
+   *
+   */
+  private static final long serialVersionUID = 7331548557628545541L;
+  private final int iD;
+  private final String title;
+  private final int releaseYear;
+  private final String publisher;
+  private final String author;
+  private final Language currentLanguage;
+  private List<ReviewImpl> setReview = new LinkedList<ReviewImpl>();
+  private Set<Integer> like;
+  private ItemGenre genre;
+  private float averageVote;
 
-    /**
-     * Item's constructors whit starter initialization. In the field ID there is
-     * the global item's IDenti
-     *
-     * @param initTitle
-     *            of the general item
-     * @param initReleaseYear
-     *            of the general item
-     * @param initPublisher
-     *            of the general item
-     * @param initAuthor
-     *            author in the case of book, director in the case of movie
-     * @param initCurrentLanguage
-     *            of the general item contained in the archive
-     */
-    public ItemImpl(final String initTitle, final int initReleaseYear, final String initPublisher,
-            final String initAuthor, final Language initCurrentLanguage, final ItemGenre initGenre) {
-        this.title = initTitle.toUpperCase();
-        this.releaseYear = initReleaseYear;
-        this.publisher = initPublisher.toUpperCase();
-        this.author = initAuthor.toUpperCase();
-        this.currentLanguage = initCurrentLanguage;
-        this.like = new HashSet<Integer>();
-        this.averageVote = 0;
-        this.iD = this.hashCode();
-        this.genre = initGenre;
+  /**
+   * Item's constructors whit starter initialization. In the field ID there is
+   * the global item's IDenti
+   *
+   * @param initTitle
+   *          of the general item
+   * @param initReleaseYear
+   *          of the general item
+   * @param initPublisher
+   *          of the general item
+   * @param initAuthor
+   *          author in the case of book, director in the case of movie
+   * @param initCurrentLanguage
+   *          of the general item contained in the archive
+   * @param initGenre
+   *          of the general item
+   * 
+   */
+  public ItemImpl(final String initTitle, final int initReleaseYear, final String initPublisher,
+              final String initAuthor, final Language initCurrentLanguage,
+              final ItemGenre initGenre) {
+    this.title = initTitle.toUpperCase();
+    this.releaseYear = initReleaseYear;
+    this.publisher = initPublisher.toUpperCase();
+    this.author = initAuthor.toUpperCase();
+    this.currentLanguage = initCurrentLanguage;
+    this.like = new HashSet<Integer>();
+    this.averageVote = 0;
+    this.iD = this.hashCode();
+    this.genre = initGenre;
+  }
+
+  @Override
+  public String toString() {
+    return "[iD=" + this.iD + ", title=" + this.title + ", releaseYear=" + this.releaseYear
+                + ", publisher=" + this.publisher + ", author=" + this.author + ", currentLanguage="
+                + this.currentLanguage + ", setReview=" + this.setReview.toString() + ", like="
+                + this.like + ", averageVote=" + this.averageVote + this.genre;
+  }
+
+  @Override
+  public String getPublisher() {
+    return this.publisher;
+  }
+
+  @Override
+  public Language getCurrentLanguage() {
+    return this.currentLanguage;
+  }
+
+  @Override
+  public int getiD() {
+    return this.iD;
+  }
+
+  @Override
+  public String getTitle() {
+    return this.title;
+  }
+
+  @Override
+  public int getReleaseYear() {
+    return this.releaseYear;
+  }
+
+  @Override
+  public String getAuthor() {
+    return this.author;
+  }
+
+  @Override
+  public List<ReviewImpl> getSetReview() {
+    return this.setReview;
+  }
+
+  @Override
+  public int getLike() {
+    return this.like.size();
+  }
+
+  @Override
+  public Set<Integer> getLikeUser() {
+    return this.like;
+  }
+
+  @Override
+  public float getAverageVote() {
+    return this.averageVote;
+  }
+
+  @Override
+  public ItemGenre getGenre() {
+    return this.genre;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(this.author, this.releaseYear, this.title, this.publisher,
+                this.currentLanguage);
+  }
+
+  @Override
+  public boolean equals(final Object object) {
+    if (!(object instanceof ItemImpl)) {
+      return false;
     }
-
-    @Override
-    public String toString() {
-        return "[iD=" + this.iD + ", title=" + this.title + ", releaseYear=" + this.releaseYear + ", publisher="
-                + this.publisher + ", author=" + this.author + ", currentLanguage=" + this.currentLanguage
-                + ", setReview=" + this.setReview.toString() + ", like=" + this.like + ", averageVote="
-                + this.averageVote + this.genre;
-    }
-
-    @Override
-    public String getPublisher() {
-        return this.publisher;
-    }
-
-    @Override
-    public Language getCurrentLanguage() {
-        return this.currentLanguage;
-    }
-
-    @Override
-    public int getiD() {
-        return this.iD;
-    }
-
-    @Override
-    public String getTitle() {
-        return this.title;
-    }
-
-    @Override
-    public int getReleaseYear() {
-        return this.releaseYear;
-    }
-
-    @Override
-    public String getAuthor() {
-        return this.author;
-    }
-
-    @Override
-    public List<ReviewImpl> getSetReview() {
-        return this.setReview;
-    }
-
-    @Override
-    public int getLike() {
-        return this.like.size();
-    }
-
-    @Override
-    public Set<Integer> getLikeUser() {
-        return this.like;
-    }
-
-    @Override
-    public float getAverageVote() {
-        return this.averageVote;
-    }
-
-    @Override
-    public ItemGenre getGenre() {
-        return this.genre;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(this.author, this.releaseYear, this.title, this.publisher, this.currentLanguage);
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (!(object instanceof ItemImpl)) {
-            return false;
-        }
-        final ItemImpl item = (ItemImpl) object;
-        return Objects.equal(this.author, item.author) && Objects.equal(this.releaseYear, item.releaseYear)
-                && Objects.equal(this.title, item.title) && Objects.equal(this.publisher, item.publisher)
+    final ItemImpl item = (ItemImpl) object;
+    return Objects.equal(this.author, item.author)
+                && Objects.equal(this.releaseYear, item.releaseYear)
+                && Objects.equal(this.title, item.title)
+                && Objects.equal(this.publisher, item.publisher)
                 && Objects.equal(this.currentLanguage, item.currentLanguage);
-    }
+  }
 
-    @Override
-    public void addReview(final ReviewImpl rev) {
-        this.getSetReview().add(rev);
-        this.setAverageVote();
-    }
+  @Override
+  public void addReview(final ReviewImpl rev) {
+    this.getSetReview().add(rev);
+    this.setAverageVote();
+  }
 
-    @Override
-    public void addLike(final Integer userId) {
-        if (!this.like.contains(userId)) {
-            this.like.add(userId);
-        }
+  @Override
+  public void addLike(final Integer userId) {
+    if (!this.like.contains(userId)) {
+      this.like.add(userId);
     }
+  }
 
-    @Override
-    public void setAverageVote() {
-        int div = this.setReview.size();
-        int total = 0;
-        for (Review r : this.setReview) {
-            total = total + r.getVote();
-        }
-        this.averageVote = (float) total / div;
+  @Override
+  public void setAverageVote() {
+    int div = this.setReview.size();
+    int total = 0;
+    for (Review r : this.setReview) {
+      total = total + r.getVote();
     }
+    this.averageVote = (float) total / div;
+  }
 }
