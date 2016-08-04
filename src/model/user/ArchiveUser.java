a711102e75a5e6cd0382093c1faf61428c94b911
Edoardo Frati <edoardo.frati2@studio.unibo.it>
2016-08-04 15:33:17 +0200

    Adding ArchiveUser interface

--- src/model/user/ArchiveUser.java
+++ src/model/user/ArchiveUser.java
@@ -1,38 +1,22 @@
 package model.user;
 
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * This class implements Serializable, it has only one field which contains the
- * map of user register to the 'Mediateca'.
+ * This interface contains methods to communicate with user archive.
  *
  * @author Edoardo
  *
  */
-public final class ArchiveUser implements Serializable {
-
-  /**
-   *
-   */
-  private static final long serialVersionUID = -891777149481744993L;
-  private static ArchiveUser singleton = null;
-  private Map<Integer, User> userArchive = new HashMap<>();
-
-  private ArchiveUser() {
-  }
+public interface ArchiveUser {
 
   /**
    * This method return the map of user into the archive.
    *
    * @return the userArchive
    */
-  public Map<Integer, User> getUserArchive() {
-    return this.userArchive;
-  }
+  Map<Integer, User> getUserArchive();
 
   /**
    * This method set the map of user into the archive.
@@ -40,9 +24,7 @@ public final class ArchiveUser implements Serializable {
    * @param initUserArchive
    *          the userArchive to set.
    */
-  public void setUserArchive(final Map<Integer, User> initUserArchive) {
-    this.userArchive = initUserArchive;
-  }
+  void setUserArchive(final Map<Integer, User> initUserArchive);
 
   /**
    * This method must be called only at the start of the program, if it had
@@ -54,25 +36,7 @@ public final class ArchiveUser implements Serializable {
    * @throws Exception
    *           in the case which field it's already initialized.
    */
-  public void setArchiveImpl(final Map<Integer, User> initUserArchive) throws Exception {
-    if (ArchiveUser.singleton == null) {
-      ArchiveUser.getArchiveImpl().setUserArchive(initUserArchive);
-    } else {
-      throw new Exception("Item archive already loaded");
-    }
-  }
-
-  /**
-   * Singleton constructor.
-   *
-   * @return the ArchiveUser.
-   */
-  public static ArchiveUser getArchiveImpl() {
-    if (ArchiveUser.singleton == null) {
-      ArchiveUser.singleton = new ArchiveUser();
-    }
-    return ArchiveUser.singleton;
-  }
+  void setArchiveImpl(final Map<Integer, User> initUserArchive) throws Exception;
 
   /**
    * This method adds a User into the archive.
@@ -82,15 +46,7 @@ public final class ArchiveUser implements Serializable {
    * @throws Exception
    *           if user is not contained into the archive.
    */
-  public void addUser(final User initUser) throws Exception {
-    if (!ArchiveUser.singleton.contains(initUser.getIdUser())) {
-      ArchiveUser.singleton.getUserArchive().put(initUser.getIdUser(), initUser);
-
-    } else {
-      throw new Exception("User: " + initUser.getIdUser()
-                  + "contained into the archive.Can not add one more time.");
-    }
-  }
+  void addUser(final User initUser) throws Exception;
 
   /**
    * This method remove/delete user from the archive.
@@ -100,13 +56,7 @@ public final class ArchiveUser implements Serializable {
    * @throws Exception
    *           if user is not contained into the archive.
    */
-  public void removeUser(final Integer userId) throws Exception {
-    if (ArchiveUser.singleton.contains(userId)) {
-      ArchiveUser.singleton.getUserArchive().remove(userId);
-    } else {
-      throw new Exception("User: " + userId + " not contained into the archive.Can not remove it");
-    }
-  }
+  void removeUser(final Integer userId) throws Exception;
 
   /**
    * This method return the required user with identifier = userId.
@@ -117,29 +67,19 @@ public final class ArchiveUser implements Serializable {
    * @throws Exception
    *           if user is not contained into the archive.
    */
-  public User getUser(final Integer userId) throws Exception {
-    if (ArchiveUser.singleton.contains(userId)) {
-      return ArchiveUser.singleton.getUserArchive().get(userId);
-    } else {
-      throw new Exception("User: " + userId + " not contained into the archive.Can not remove it");
-    }
-  }
+  User getUser(final Integer userId) throws Exception;
 
   /**
    * @param userId
    *          to find into the archive.
    * @return true if userId is into the archive, else false.
    */
-  public boolean contains(final Integer userId) {
-    return ArchiveUser.singleton.getUserArchive().containsKey(userId);
-  }
+  boolean contains(final Integer userId);
 
   /**
    * This method return a Set of all UserId in the archive.
    *
    * @return UserId set.
    */
-  public Set<Integer> getUserId() {
-    return Collections.unmodifiableSet(ArchiveUser.singleton.getUserArchive().keySet());
-  }
+  Set<Integer> getUserId();
 }
---
+++ src/model/user/ArchiveUserImpl.java
@@ -0,0 +1,97 @@
+package model.user;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This class implements Serializable, it has only one field which contains the
+ * map of user register to the 'Mediateca'.
+ *
+ * @author Edoardo
+ *
+ */
+public final class ArchiveUserImpl implements Serializable, ArchiveUser {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = -891777149481744993L;
+  private static ArchiveUserImpl singleton = null;
+  private Map<Integer, User> userArchive = new HashMap<>();
+
+  private ArchiveUserImpl() {
+  }
+
+  @Override
+  public Map<Integer, User> getUserArchive() {
+    return this.userArchive;
+  }
+
+  @Override
+  public void setUserArchive(final Map<Integer, User> initUserArchive) {
+    this.userArchive = initUserArchive;
+  }
+
+  @Override
+  public void setArchiveImpl(final Map<Integer, User> initUserArchive) throws Exception {
+    if (ArchiveUserImpl.singleton == null) {
+      ArchiveUserImpl.getArchiveImpl().setUserArchive(initUserArchive);
+    } else {
+      throw new Exception("Item archive already loaded");
+    }
+  }
+
+  /**
+   * Singleton constructor.
+   *
+   * @return the ArchiveUser.
+   */
+  public static ArchiveUserImpl getArchiveImpl() {
+    if (ArchiveUserImpl.singleton == null) {
+      ArchiveUserImpl.singleton = new ArchiveUserImpl();
+    }
+    return ArchiveUserImpl.singleton;
+  }
+
+  @Override
+  public void addUser(final User initUser) throws Exception {
+    if (!ArchiveUserImpl.singleton.contains(initUser.getIdUser())) {
+      ArchiveUserImpl.singleton.getUserArchive().put(initUser.getIdUser(), initUser);
+
+    } else {
+      throw new Exception("User: " + initUser.getIdUser()
+                  + "contained into the archive.Can not add one more time.");
+    }
+  }
+
+  @Override
+  public void removeUser(final Integer userId) throws Exception {
+    if (ArchiveUserImpl.singleton.contains(userId)) {
+      ArchiveUserImpl.singleton.getUserArchive().remove(userId);
+    } else {
+      throw new Exception("User: " + userId + " not contained into the archive.Can not remove it");
+    }
+  }
+
+  @Override
+  public User getUser(final Integer userId) throws Exception {
+    if (ArchiveUserImpl.singleton.contains(userId)) {
+      return ArchiveUserImpl.singleton.getUserArchive().get(userId);
+    } else {
+      throw new Exception("User: " + userId + " not contained into the archive.Can not remove it");
+    }
+  }
+
+  @Override
+  public boolean contains(final Integer userId) {
+    return ArchiveUserImpl.singleton.getUserArchive().containsKey(userId);
+  }
+
+  @Override
+  public Set<Integer> getUserId() {
+    return Collections.unmodifiableSet(ArchiveUserImpl.singleton.getUserArchive().keySet());
+  }
+}
