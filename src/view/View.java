faba160024d604063408705e4bbe17ea7eda6782
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-08-05 17:48:52 +0200

    Added view and view implementation, with empty methods. Start thinking at CardLayout

---
+++ src/view/View.java
@@ -0,0 +1,99 @@
+package view;
+
+/**
+ *
+ * Interface for the view. It defines methods which can be used by the
+ * controller.
+ * 
+ * @author Luca Giorgetti
+ */
+public interface View {
+	/**
+	 * method which @return the typed Username in login.
+	 */
+	String getUsername();
+
+	/**
+	 *
+	 * method which @return the typed Password in login.
+	 */
+	String getPassword();
+
+	/**
+	 *
+	 * method which @return the word which the user wants to search.
+	 */
+	String getSearchFilter();
+
+	/**
+	 *
+	 * method which @return the review typed by the user.
+	 */
+	String getReview();
+
+	/**
+	 *
+	 * method which sets the list of items filtered by the word typed .
+	 */
+	void setFilteredList();
+
+	/**
+	 *
+	 * method which sets the list of items borrowed by user.
+	 */
+	void setBorrowedItemList();
+
+	/**
+	 *
+	 * method which @return the id of borrowed by user.
+	 */
+	int getBorrowedItem();
+
+	/**
+	 *
+	 * method which @return the id of given back by user.
+	 */
+	int getGivenBackItem();
+
+	/**
+	 *
+	 * method which sets the list of available items.
+	 */
+	void setItemAvailabilityList();
+
+	/**
+	 *
+	 * method which @return informations from user registration.
+	 */
+	void getUserRegistration();
+
+	/**
+	 *
+	 * method which @return the password typed by manager.
+	 */
+	String getMenagerPassword();
+
+	/**
+	 *
+	 * method which @return information from item creation.
+	 */
+	void getAddedItem();
+
+	/**
+	 *
+	 * method which @return information from item removal.
+	 */
+	void getRemovedItem();
+
+	/**
+	 *
+	 * method which @return the numeber of taken sit.
+	 */
+	int getStudyRoomSitsJustTaken();
+
+	/**
+	 *
+	 * method which sets the status of study room.
+	 */
+	void setStudyRoomStatus();
+}
---
+++ src/view/ViewImpl.java
@@ -0,0 +1,85 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main class of the view. Contains the implementation of methods which can be
+ * used by controller.
+ * 
+ * @author Luca
+ *
+ */
+public class ViewImpl implements View {
+
+	@Override
+	public String getUsername() {
+
+	}
+
+	@Override
+	public String getPassword() {
+
+	}
+
+	@Override
+	public String getSearchFilter() {
+
+	}
+
+	@Override
+	public String getReview() {
+
+	}
+
+	public void setFilteredList(final List filteredList) {
+
+	}
+
+	public void setBorrowedItemList(final List borrowedItemsList) {
+
+	}
+
+	@Override
+	public int getBorrowedItem() {
+
+	}
+
+	@Override
+	public int getGivenBackItem() {
+
+	}
+
+	public void setItemAvailabilityList(final List availableItemList) {
+
+	}
+
+	@Override
+	public void getUserRegistration() {
+
+	}
+
+	@Override
+	public String getMenagerPassword() {
+
+	}
+
+	@Override
+	public void getAddedItem() {
+
+	}
+
+	@Override
+	public void getRemovedItem() {
+
+	}
+
+	@Override
+	public int getStudyRoomSitsJustTaken() {
+
+	}
+
+	public void setStudyRoomStatus(final ArrayList studyRoomStatus) {
+
+	}
+}
