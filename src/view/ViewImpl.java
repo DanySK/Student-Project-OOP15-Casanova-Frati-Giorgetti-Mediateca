060ff14e80d86da4a9d029f914b75c784dd01b20
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-08-19 22:48:49 +0200

    Minor Fix

--- src/view/ViewImpl.java
+++ src/view/ViewImpl.java
@@ -80,12 +80,12 @@ public class ViewImpl implements View {
 	public enum CardName {
 		MAIN("Main Card"), LOGIN("Login Card"), MENU("Menu Card"), ITEM(
 				"Item Card"), USER_MODIFY("User Modify Card"), LIKE_LIST(
-				"LikeList Screen Card"), BORROWED_LIST(
-				"BorrowedList Screen Card"), REVIEW("Review Card"), USER_CREATE(
-				"User Create Card"), MANAGER_LOGIN("Manager Login"), ITEM_CREATE(
-				"Item Create Card"), MANAGER_MENU("Manager Menu Card"), STUDY_ROOM(
-				"Study Room Card"), WISHLIST("Wishlist Card"), ITEM_MODIFY(
-				"Item Modify Card");
+						"LikeList Screen Card"), BORROWED_LIST(
+								"BorrowedList Screen Card"), REVIEW("Review Card"), USER_CREATE(
+										"User Create Card"), MANAGER_LOGIN("Manager Login"), ITEM_CREATE(
+												"Item Create Card"), MANAGER_MENU("Manager Menu Card"), STUDY_ROOM(
+														"Study Room Card"), WISHLIST("Wishlist Card"), ITEM_MODIFY(
+																"Item Modify Card");
 
 		private final String name;
 
@@ -117,11 +117,9 @@ public class ViewImpl implements View {
 		this.card7 = new ReviewScreenImpl(this);
 		this.card8 = new UserScreenImpl(this, UserScreenType.CREATE);
 		this.card9 = new UserLoginImpl(this, LoginType.MANAGER);
-		this.card10 = new ItemScreenImpl(this, ItemScreenType.CREATE,
-				ViewImpl.SCREEN_LENGHT, ViewImpl.SCREEN_LENGHT);
+		this.card10 = new ItemScreenImpl(this, ItemScreenType.CREATE);
 		this.card11 = new StudyRoomImpl(this);
-		this.card13 = new ItemScreenImpl(this, ItemScreenType.MODIFY,
-				ViewImpl.SCREEN_LENGHT, ViewImpl.SCREEN_WIDTH);
+		this.card13 = new ItemScreenImpl(this, ItemScreenType.MODIFY);
 		this.card12 = new ManagerScreenImpl(this);
 
 		this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -371,8 +369,8 @@ public class ViewImpl implements View {
 			final String duration, final utils.TypeColor color,
 			final utils.Language language) {
 		((ItemScreenImpl) this.card10)
-		.setFilmField(title, author, manifacturer, year, genre,
-				imagePath, duration, color, language);
+				.setFilmField(title, author, manifacturer, year, genre,
+						imagePath, duration, color, language);
 	}
 
 	// //waiting CONTROLLER function name
@@ -551,10 +549,10 @@ public class ViewImpl implements View {
 	public void showGiveBackOptionMessage(final String book) {
 		// Custom button text
 		final Object[] options = { "Consegna",
-				"Aumenta il prestito di un altro mese" };
+		"Aumenta il prestito di un altro mese" };
 		int choose = JOptionPane.showOptionDialog(this.mainFrame,
 				"Dovresti consegare il seguente libro:" + book
-				+ "Cosa vuoi fare?", "Notifica di consegna",
+						+ "Cosa vuoi fare?", "Notifica di consegna",
 				JOptionPane.YES_NO_CANCEL_OPTION, 0, null, options, options[0]);
 
 		if (choose == 0) {
@@ -600,4 +598,5 @@ public class ViewImpl implements View {
 	public String getItemSelectedByUser() {
 		return ((MediatecaScreenImpl) this.card3).getSelectedItemFromList();
 	}
+
 }
