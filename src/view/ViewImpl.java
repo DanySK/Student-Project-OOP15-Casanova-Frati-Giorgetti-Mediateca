0382eaffcf4634e02e7caba12d68566ff83ab344
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-08-20 09:13:22 +0200

    Added first connection to controller

--- src/view/ViewImpl.java
+++ src/view/ViewImpl.java
@@ -81,12 +81,12 @@ public class ViewImpl implements View {
 	public enum CardName {
 		MAIN("Main Card"), LOGIN("Login Card"), MENU("Menu Card"), ITEM(
 				"Item Card"), USER_MODIFY("User Modify Card"), LIKE_LIST(
-				"LikeList Screen Card"), BORROWED_LIST(
-				"BorrowedList Screen Card"), REVIEW("Review Card"), USER_CREATE(
-				"User Create Card"), MANAGER_LOGIN("Manager Login"), ITEM_CREATE(
-				"Item Create Card"), MANAGER_MENU("Manager Menu Card"), STUDY_ROOM(
-				"Study Room Card"), WISHLIST("Wishlist Card"), ITEM_MODIFY(
-				"Item Modify Card"), ALL_REVIEWS("All Reviews Card");
+						"LikeList Screen Card"), BORROWED_LIST(
+								"BorrowedList Screen Card"), REVIEW("Review Card"), USER_CREATE(
+										"User Create Card"), MANAGER_LOGIN("Manager Login"), ITEM_CREATE(
+												"Item Create Card"), MANAGER_MENU("Manager Menu Card"), STUDY_ROOM(
+														"Study Room Card"), WISHLIST("Wishlist Card"), ITEM_MODIFY(
+																"Item Modify Card"), ALL_REVIEWS("All Reviews Card");
 
 		private final String name;
 
@@ -250,20 +250,27 @@ public class ViewImpl implements View {
 
 	@Override
 	public void borrowItem() {
+		// c.borrow();
 	}
 
 	@Override
 	public void giveBackItem() {
+		// c.giveBack();
 	}
 
+	// //OK
 	@Override
 	public void reviewItem() {
+		this.c.addReview();
 	}
 
+	// //OK
 	@Override
 	public void likeItem() {
+		this.c.addLike();
 	}
 
+	// //OK
 	@Override
 	public Object getUserRegistration(final utils.UserInfo info) {
 		return ((UserScreenImpl) this.card8).getInfo(info);
@@ -276,6 +283,7 @@ public class ViewImpl implements View {
 		this.c.userLogin();
 	}
 
+	// //OK
 	@Override
 	public String getMenagerPassword() {
 		return ((UserLoginImpl) this.card9).getManagerPassword();
@@ -308,10 +316,9 @@ public class ViewImpl implements View {
 		return ((UserScreenImpl) this.card4).getInfo(info);
 	}
 
-	// //waiting CONTROLLER function name
 	@Override
 	public void giveMeBorrowList() {
-		// ViewImpl.c.borrowList();
+		this.c.borrowList();
 	}
 
 	// //waiting CONTROLLER function name
@@ -321,7 +328,7 @@ public class ViewImpl implements View {
 
 	}
 
-	// //waiting CONTROLLER function name
+	// //OK
 	@Override
 	public void sendUserCreate() {
 		this.c.registerNewUser();
@@ -335,10 +342,10 @@ public class ViewImpl implements View {
 
 	}
 
-	// //waiting CONTROLLER function name
+	// //OK
 	@Override
 	public void giveMeFilteredList() {
-		// this.c.itemElaboration();
+		this.c.itemElaboration();
 	}
 
 	// //waiting CONTROLLER function name
@@ -372,8 +379,8 @@ public class ViewImpl implements View {
 			final String duration, final utils.TypeColor color,
 			final utils.Language language) {
 		((ItemScreenImpl) this.card10)
-		.setFilmField(title, author, manifacturer, year, genre,
-				imagePath, duration, color, language);
+				.setFilmField(title, author, manifacturer, year, genre,
+						imagePath, duration, color, language);
 	}
 
 	// //waiting CONTROLLER function name
@@ -409,16 +416,16 @@ public class ViewImpl implements View {
 		JOptionPane.showMessageDialog(this.mainFrame, message);
 	}
 
-	// //waiting CONTROLLER function name
+	// //OK
 	@Override
 	public void takeSit() {
-		// this.c.takeSit();
+		this.c.takeSit();
 	}
 
-	// //waiting CONTROLLER function name
+	// //OK
 	@Override
 	public void giveMeStudyRoomStatus() {
-		// this.c.studyRoomStatus();
+		this.c.studyRoomStatus();
 	}
 
 	@Override
@@ -447,10 +454,10 @@ public class ViewImpl implements View {
 		return ((StudyRoomImpl) this.card11).getDateYear();
 	}
 
-	// //waiting CONTROLLER function name
+	// //OK
 	@Override
 	public void removeSit() {
-		// c.removeSit();
+		this.c.cancelSit();
 
 	}
 
@@ -526,7 +533,7 @@ public class ViewImpl implements View {
 	// //waiting CONTROLLER function name
 	@Override
 	public void deleteUser() {
-		// c.deleteUser;
+		// this.c.deleteUser();
 
 	}
 
@@ -552,10 +559,10 @@ public class ViewImpl implements View {
 	public void showGiveBackOptionMessage(final String book) {
 		// Custom button text
 		final Object[] options = { "Consegna",
-				"Aumenta il prestito di un altro mese" };
+		"Aumenta il prestito di un altro mese" };
 		int choose = JOptionPane.showOptionDialog(this.mainFrame,
 				"Dovresti consegare il seguente libro:" + book
-				+ "Cosa vuoi fare?", "Notifica di consegna",
+						+ "Cosa vuoi fare?", "Notifica di consegna",
 				JOptionPane.YES_NO_CANCEL_OPTION, 0, null, options, options[0]);
 
 		if (choose == 0) {
