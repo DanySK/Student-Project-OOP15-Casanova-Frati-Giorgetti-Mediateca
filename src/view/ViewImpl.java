201ae58919ca9467f7d83b2fde78800b5f7d8eab
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-08-28 17:57:16 +0200

    Tried review

--- src/view/ViewImpl.java
+++ src/view/ViewImpl.java
@@ -100,13 +100,13 @@ public class ViewImpl implements View {
 	public enum CardName {
 		START("Start Card"), MAIN("Main Card"), LOGIN("Login Card"), MENU(
 				"Menu Card"), ITEM("Item Card"), USER_MODIFY("User Modify Card"), LIKE_LIST(
-						"LikeList Screen Card"), BORROWED_LIST(
-								"BorrowedList Screen Card"), REVIEW("Review Card"), USER_CREATE(
-										"User Create Card"), MANAGER_LOGIN("Manager Login"), BOOK_CREATE(
-												"Book Create Card"), FILM_CREATE("Film Create Card"), MANAGER_MENU(
-														"Manager Menu Card"), STUDY_ROOM("Study Room Card"), WISHLIST(
-																"Wishlist Card"), BOOK_MODIFY("Book Modify Card"), FILM_MODIFY(
-																		"Film Modify Card"), ALL_REVIEWS("All Reviews Card");
+				"LikeList Screen Card"), BORROWED_LIST(
+				"BorrowedList Screen Card"), REVIEW("Review Card"), USER_CREATE(
+				"User Create Card"), MANAGER_LOGIN("Manager Login"), BOOK_CREATE(
+				"Book Create Card"), FILM_CREATE("Film Create Card"), MANAGER_MENU(
+				"Manager Menu Card"), STUDY_ROOM("Study Room Card"), WISHLIST(
+				"Wishlist Card"), BOOK_MODIFY("Book Modify Card"), FILM_MODIFY(
+				"Film Modify Card"), ALL_REVIEWS("All Reviews Card");
 
 		private final String name;
 
@@ -643,10 +643,10 @@ public class ViewImpl implements View {
 	public void showGiveBackOptionMessage(final String book) {
 		// Custom button text
 		final Object[] options = { "Consegna",
-		"Aumenta il prestito di un altro mese" };
+				"Aumenta il prestito di un altro mese" };
 		int choose = JOptionPane.showOptionDialog(this.mainFrame,
 				"Dovresti consegare il seguente libro:" + book
-				+ "Cosa vuoi fare?", "Notifica di consegna",
+						+ "Cosa vuoi fare?", "Notifica di consegna",
 				JOptionPane.YES_NO_CANCEL_OPTION, 0, null, options, options[0]);
 
 		if (choose == 0) {
