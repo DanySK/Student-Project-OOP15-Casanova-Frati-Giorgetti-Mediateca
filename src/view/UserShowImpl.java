625576428403055abffa7360cfbaf01f22da0ba6
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-08-31 17:59:41 +0200

    Fixed field in usershow

--- src/view/UserShowImpl.java
+++ src/view/UserShowImpl.java
@@ -31,6 +31,9 @@ public class UserShowImpl implements UserShow {
 	private String email;
 	private String birthDate;
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	@Override
 	public void startUserShow(final View v) {
 		v.giveMeItemInfoMediateca();
@@ -54,81 +57,81 @@ public class UserShowImpl implements UserShow {
 
 		final JLabel surnameL = new JLabel();
 		surnameL.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
-		surnameL.setBounds(20, 41, 362, 25);
+		surnameL.setBounds(20, 44, 362, 25);
 		surnameL.setText("Cognome: " + this.surname);
 		mainFrame.getContentPane().add(surnameL);
 
 		final JLabel usernameL = new JLabel("Username: " + this.username);
 		usernameL.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
-		usernameL.setBounds(20, 70, 362, 25);
+		usernameL.setBounds(20, 74, 362, 25);
 		mainFrame.getContentPane().add(usernameL);
 
 		final JLabel passwordL = new JLabel("Password: " + this.password);
 		passwordL.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
-		passwordL.setBounds(20, 101, 362, 25);
+		passwordL.setBounds(20, 104, 362, 25);
 		mainFrame.getContentPane().add(passwordL);
 
 		final JLabel telephoneL = new JLabel("Telefono: " + this.telephone);
 		telephoneL.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
-		telephoneL.setBounds(20, 181, 362, 25);
+		telephoneL.setBounds(20, 194, 362, 25);
 		mainFrame.getContentPane().add(telephoneL);
 
 		final JLabel emailL = new JLabel("Email: " + this.email);
 		emailL.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
-		emailL.setBounds(20, 128, 362, 25);
+		emailL.setBounds(20, 134, 362, 25);
 		mainFrame.getContentPane().add(emailL);
 
 		final JLabel birthDateL = new JLabel("Data di nascita: "
 				+ this.birthDate);
 		birthDateL.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
-		birthDateL.setBounds(20, 155, 362, 25);
+		birthDateL.setBounds(20, 164, 362, 25);
 		mainFrame.getContentPane().add(birthDateL);
 
 		final JLabel bookPref1L = new JLabel(this.bookPref1.toString());
 		bookPref1L.setHorizontalAlignment(SwingConstants.CENTER);
-		bookPref1L.setBounds(20, 252, 362, 25);
+		bookPref1L.setBounds(20, 254, 362, 25);
 		bookPref1L.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
 		mainFrame.getContentPane().add(bookPref1L);
 
 		final JLabel bookPref2L = new JLabel(this.bookPref2.toString());
 		bookPref2L.setHorizontalAlignment(SwingConstants.CENTER);
-		bookPref2L.setBounds(20, 277, 362, 25);
+		bookPref2L.setBounds(20, 284, 362, 25);
 		bookPref2L.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
 		mainFrame.getContentPane().add(bookPref2L);
 
 		final JLabel bookPref3L = new JLabel(this.bookPref3.toString());
 		bookPref3L.setHorizontalAlignment(SwingConstants.CENTER);
-		bookPref3L.setBounds(20, 304, 362, 25);
+		bookPref3L.setBounds(20, 314, 362, 25);
 		bookPref3L.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
 		mainFrame.getContentPane().add(bookPref3L);
 
 		final JLabel filmPref1L = new JLabel(this.filmPref1.toString());
 		filmPref1L.setHorizontalAlignment(SwingConstants.CENTER);
-		filmPref1L.setBounds(20, 354, 362, 25);
+		filmPref1L.setBounds(20, 374, 362, 25);
 		filmPref1L.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
 		mainFrame.getContentPane().add(filmPref1L);
 
 		final JLabel filmPref2L = new JLabel(this.filmPref2.toString());
 		filmPref2L.setHorizontalAlignment(SwingConstants.CENTER);
-		filmPref2L.setBounds(20, 378, 362, 25);
+		filmPref2L.setBounds(20, 404, 362, 25);
 		filmPref2L.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
 		mainFrame.getContentPane().add(filmPref2L);
 
 		final JLabel filmPref3L = new JLabel(this.filmPref3.toString());
 		filmPref3L.setHorizontalAlignment(SwingConstants.CENTER);
-		filmPref3L.setBounds(20, 402, 362, 25);
+		filmPref3L.setBounds(20, 434, 362, 25);
 		filmPref3L.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.SMALL_SIZE));
 
 		mainFrame.getContentPane().add(filmPref3L);
 
 		JLabel bookPreferences = new JLabel("Preferenze genere libri:");
-		bookPreferences.setBounds(20, 227, 362, 25);
+		bookPreferences.setBounds(20, 224, 362, 25);
 		bookPreferences.setFont(new Font("Tahoma", Font.PLAIN,
 				ViewImpl.SMALL_SIZE));
 		mainFrame.getContentPane().add(bookPreferences);
 
 		JLabel filmPreferences = new JLabel("Preferenze genere film:");
-		filmPreferences.setBounds(20, 330, 362, 25);
+		filmPreferences.setBounds(20, 344, 362, 25);
 		filmPreferences.setFont(new Font("Tahoma", Font.PLAIN,
 				ViewImpl.SMALL_SIZE));
 		mainFrame.getContentPane().add(filmPreferences);
