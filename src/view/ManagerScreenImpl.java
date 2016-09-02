a14c7953c0004c6fe8d283612e61c3ea06ac12c5
Luca Giorgetti <luca.giorgetti7@studio.unibo.it>
2016-09-02 16:44:32 +0200

    Disabled buttons at starting of manager menu

--- src/view/ManagerScreenImpl.java
+++ src/view/ManagerScreenImpl.java
@@ -77,11 +77,14 @@ public class ManagerScreenImpl extends JPanel implements ManagerScreen {
 		modifyMovie.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.FONT_SIZE));
 		modifyMovie.setBounds(498, 350, 273, 40);
 		this.add(modifyMovie);
-
+		seeBorrowedList.setEnabled(false);
+		delete.setEnabled(false);
+		modifyBook.setEnabled(false);
+		modifyMovie.setEnabled(false);
 		JButton showUserList = new JButton("Lista Utenti");
 		showUserList.setBounds(498, 150, 273, 40);
 		showUserList
-				.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.FONT_SIZE));
+		.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.FONT_SIZE));
 		this.add(showUserList);
 		// SHOW ALL USER LIST -> REFRESH
 		showUserList.addActionListener(e -> {
@@ -97,7 +100,7 @@ public class ManagerScreenImpl extends JPanel implements ManagerScreen {
 		JButton showItemList = new JButton("Lista Oggetti");
 		showItemList.setBounds(498, 200, 273, 40);
 		showItemList
-				.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.FONT_SIZE));
+		.setFont(new Font("Tahoma", Font.PLAIN, ViewImpl.FONT_SIZE));
 		this.add(showItemList);
 		// SHOW ALL ITEM LIST -> REFRESH
 		showItemList.addActionListener(e -> {
@@ -122,7 +125,7 @@ public class ManagerScreenImpl extends JPanel implements ManagerScreen {
 						&& (ManagerScreenImpl.this.type == TypeList.USER)) {
 					System.out.println("Cliccato utente"
 							+ ((JList) evt.getSource()).getSelectedValue()
-									.toString());
+							.toString());
 					ManagerScreenImpl.this.dClicked = ((JList) evt.getSource())
 							.getSelectedValue().toString();
 					v.showUserInfo();
@@ -130,7 +133,7 @@ public class ManagerScreenImpl extends JPanel implements ManagerScreen {
 						&& (ManagerScreenImpl.this.type == TypeList.ITEM)) {
 					System.out.println("Cliccato utente"
 							+ ((JList) evt.getSource()).getSelectedValue()
-									.toString());
+							.toString());
 					ManagerScreenImpl.this.dClicked = ((JList) evt.getSource())
 							.getSelectedValue().toString();
 					v.showItemInfoManager();
