74da9481c1d663988789b6b527049f7199884a8f
Edoardo Frati <edoardo.frati2@studio.unibo.it>
2016-09-06 10:12:21 +0200

    Fixed toString()

--- src/model/item/ReviewImpl.java
+++ src/model/item/ReviewImpl.java
@@ -11,73 +11,73 @@ import java.util.GregorianCalendar;
  */
 public class ReviewImpl implements Review, Serializable {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 8843824865824137318L;
-    private static final int MAX = 5;
-    private static final int MIN = 0;
-    private static final int RAN = 20;
-    private int vote;
-    private String note;
-    private final long id;
+  /**
+   *
+   */
+  private static final long serialVersionUID = 8843824865824137318L;
+  private static final int MAX = 5;
+  private static final int MIN = 0;
+  private static final int RAN = 20;
+  private int vote;
+  private String note;
+  private final long id;
 
-    /**
-     * Review Constructor.
-     *
-     * @param initVote
-     *            item's vote
-     * @param initNote
-     *            item's text note.
-     */
-    public ReviewImpl(final int initVote, final String initNote) {
-        this.setVote(initVote);
-        this.setNote(initNote);
-        this.id = this.myHash();
+  @Override
+  public String toString() {
+    return "[ " + this.getVote() + " | " + this.getNote() + " ]";
+  }
+
+  /**
+   * Review Constructor.
+   *
+   * @param initVote
+   *          item's vote
+   * @param initNote
+   *          item's text note.
+   */
+  public ReviewImpl(final int initVote, final String initNote) {
+    this.setVote(initVote);
+    this.setNote(initNote);
+    this.id = this.myHash();
+  }
+
+  @Override
+  public boolean setVote(final int initVote) {
+    if (this.checkVote(initVote)) {
+      this.vote = initVote;
+      return true;
     }
+    return false;
+  }
 
-    @Override
-    public boolean setVote(final int initVote) {
-        if (this.checkVote(initVote)) {
-            this.vote = initVote;
-            return true;
-        }
-        return false;
-    }
+  @Override
+  public void setNote(final String initNote) {
+    this.note = initNote.toUpperCase();
 
-    @Override
-    public void setNote(final String initNote) {
-        this.note = initNote.toUpperCase();
+  }
 
-    }
+  @Override
+  public int getVote() {
+    return this.vote;
+  }
 
-    @Override
-    public int getVote() {
-        return this.vote;
-    }
+  @Override
+  public String getNote() {
+    return this.note;
+  }
 
-    @Override
-    public String getNote() {
-        return this.note;
-    }
+  @Override
+  public boolean checkVote(final int initVote) {
+    return ((initVote >= ReviewImpl.MIN) && (initVote <= ReviewImpl.MAX));
+  }
 
-    @Override
-    public String toString() {
-        return "ReviewImpl [vote=" + this.vote + ", note=" + this.note + "]";
-    }
+  @Override
+  public long getId() {
+    return this.id;
+  }
 
-    @Override
-    public boolean checkVote(final int initVote) {
-        return ((initVote >= ReviewImpl.MIN) && (initVote <= ReviewImpl.MAX));
-    }
-
-    @Override
-    public long getId() {
-        return this.id;
-    }
-
-    private long myHash() {
-        GregorianCalendar c = new GregorianCalendar();
-        return ReviewImpl.RAN * c.getTimeInMillis();
-    }
+  private long myHash() {
+    GregorianCalendar c = new GregorianCalendar();
+    return ReviewImpl.RAN * c.getTimeInMillis();
+  }
 }
